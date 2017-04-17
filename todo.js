deb15f45b336791aa15c5e4b75d89ef0f478d6ad
parelhutahaean <parel.hutahaean@gmail.com>
2017-04-17 10:47:30 +0700

    Parel Hutahaean finished

--- todo.js
+++ todo.js
@@ -1,3 +1,17 @@
 'use strict';
 
-//write your code here
+const db = require('./models');
+const repl = require('repl');
+let replServer = repl.start({prompt: '> '});
+
+let insertTask = (name, completed) => {
+  db.Task.create({"name":name, "completed": completed})
+  .then(task => {
+    console.log(JSON.stringify(task.toJSON(), null, 2));
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
+
+replServer.context.insertTask = insertTask;
